90883e6232e82fc1ad5ac6fa9fabbd71d10a7646
rohitpim1 <pimrohit33@gmail.com>
2020-10-08 21:41:01 +0530

    Updates

---
+++ precache-manifest.3925ba123296d8e358900fedfc0ff736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac3480c904c6213fac0592ae4d60350",
+    "url": "/DailySchedule/index.html"
+  },
+  {
+    "revision": "1b4a3af2dc92b62ae859",
+    "url": "/DailySchedule/static/css/main.ab7b8948.chunk.css"
+  },
+  {
+    "revision": "8e0c02502488abbaa19d",
+    "url": "/DailySchedule/static/js/2.b5372257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DailySchedule/static/js/2.b5372257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4a3af2dc92b62ae859",
+    "url": "/DailySchedule/static/js/main.5db395be.chunk.js"
+  },
+  {
+    "revision": "0798fdd9c99d9c22708b",
+    "url": "/DailySchedule/static/js/runtime-main.adde0215.js"
+  }
+]);
